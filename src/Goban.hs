b37eaae7ede6ce716f65d3a417583b7da9ea7d2c
Jim Snavely <ludflu@gmail.com>
2024-07-07 19:19:52 -0400

    calc neighbors

--- src/Goban.hs
+++ src/Goban.hs
@@ -1,4 +1,3 @@
--- {-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE TypeFamilies              #-}
 
@@ -15,9 +14,36 @@ import           Prelude                     hiding (getContents, (!!))
 import Data.Maybe
 import qualified Data.Map as M
 
-data GoStone = Black | White deriving (Eq, Show)
+--this module is used to calculate what stones to
+-- take off the board when a new stone is placed
 
-newtype BoardState = BoardState { 
-  board :: M.Map (Int,Int) GoStone
+data GoStone = Black | White deriving (Eq, Show)
+data GoPoint =  GoPoint { x::Int, y::Int }deriving (Eq, Show)
+data BoardState = BoardState { 
+  board :: M.Map (Int,Int) GoStone,
+  boardSize :: Int
 } deriving (Show)
+
+isOnBoard :: BoardState -> GoPoint ->  Bool
+isOnBoard boardState (GoPoint x y)  = x >= 0 && x < boardSize boardState && y >= 0 && y < boardSize boardState 
+
+neighbors :: BoardState -> GoPoint -> [GoPoint]
+neighbors boardState (GoPoint x y) = 
+  let ns = [GoPoint (x+1) y, GoPoint (x-1) y, GoPoint x (y+1), GoPoint x (y-1)]
+   in filter (isOnBoard boardState) ns
+
+-- given boardState and a point, 
+-- return the list of points 
+-- connected by same-colored stones
+findDragon :: BoardState -> GoPoint -> [GoPoint]
+findDragon boardState point = []
+
+-- given boardState and a point,
+-- find all adjacent enemy dragons
+findAdjacentEnemyDragons :: BoardState -> GoPoint -> [[GoPoint]]
+findAdjacentEnemyDragons boardState point = []
+
+calcLiberties :: BoardState -> GoPoint -> Int
+calcLiberties boardState point = 0
+
 
