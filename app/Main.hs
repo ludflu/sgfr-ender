c2d26ebbf87161c85e7e51c69ab2fc8053f38126
Jim Snavely <ludflu@gmail.com>
2024-07-09 12:31:16 -0400

    render it for reals!

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,7 @@ import           Grid                        (kifu)
 import           Diagrams.Backend.Rasterific (B, renderPdf)
 import Diagrams.TwoD (dims2D)
 import SgfReader (readSgf, showMoves, renderReady)
-import Goban ( GoStone(White, Black), emptyBoard, playMoves, getAllStones )
+import Goban ( GoStone(White, Black), emptyBoard, playMoves, getAllStones, GoPoint (x, y) )
 import qualified Data.SGF as SGF
 import Control.Monad.State ( execState )
 import Diagrams (Renderable)
@@ -20,6 +20,9 @@ convertToMoves = map (\(color, (x,y)) -> (if color == SGF.Black then Black else 
 mygoban :: Renderable (Path V2 Double) B => [(SGF.Color, (Integer,Integer))] -> Int  -> QDiagram B V2 Double Any
 mygoban = kifu
 
+stonePlacement:: [(GoPoint,GoStone)] -> [ (SGF.Color, (Integer, Integer))]
+stonePlacement = map (\(point, stone) -> (if stone == Black then SGF.Black else SGF.White, ( toInteger $ x point, toInteger $  y point)))
+
 main :: IO ()
 main = do
   sgf <- readSgf "65761210-307-mannesmann-ludflu215.sgf"
@@ -29,6 +32,7 @@ main = do
       initialGoban = emptyBoard boardSize
       finalBoard = execState (playMoves gobanMoves) initialGoban
       onlyStones = getAllStones finalBoard
-      kifuDiagram = mygoban moves boardSize
+      gomoves = stonePlacement onlyStones 
+      kifuDiagram = mygoban gomoves boardSize
   print $ show onlyStones
   renderPdf 200 200 "output.pdf" (dims2D 200 200) kifuDiagram
