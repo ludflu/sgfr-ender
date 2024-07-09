92d9efeb083eb636e30d8ea38e0bc4b8d528b86a
Jim Snavely <ludflu@gmail.com>
2024-07-08 21:39:56 -0400

    another test

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -25,6 +25,21 @@ pairStoneLiberties = do placeStone (GoPoint 5 5) Black
                         placeStone (GoPoint 5 6) Black
                         libertyCount (GoPoint 5 5)
 
+
+atariStone :: State BoardState Int
+atariStone = do placeStone (GoPoint 5 5) Black
+                placeStone (GoPoint 4 5) White
+                placeStone (GoPoint 6 5) White
+                placeStone (GoPoint 5 4) White
+                placeStone (GoPoint 5 6) White
+                libertyCount (GoPoint 5 5)
+
+cornerStone = do placeStone (GoPoint 0 0) Black
+                 libertyCount (GoPoint 0 0)
+
+sideStone = do placeStone (GoPoint 5 0) Black
+               libertyCount (GoPoint 5 0)
+
 allTests :: TestTree
 allTests = testGroup "all tests" [testCalcBoundary]
 
@@ -36,8 +51,19 @@ testCalcBoundary =
         testCase "we should find a dragon" $ evalState makeDragon emptyBoard @?= 
             [GoPoint 5 5, GoPoint 6 5, GoPoint 7 5, GoPoint 8 5],
 
-        -- testCase "a lone stone should have 4 liberties" $ evalState stoneLiberties emptyBoard @?= 4,
+        testCase "a lone stone should have 4 liberties" $ 
+            evalState stoneLiberties emptyBoard @?= 4,
     
-        testCase "two stone should have 6 liberties" $ evalState pairStoneLiberties emptyBoard @?= 3
+        testCase "two stones should have 6 liberties" $ 
+            evalState pairStoneLiberties emptyBoard @?= 6,
+
+        testCase "a stone in atari should have zero liberties" $ 
+            evalState atariStone emptyBoard @?= 0,
+
+        testCase "a stone in the corner should have 2 liberties" $ 
+            evalState cornerStone emptyBoard @?= 2,
+
+        testCase "a side stone should have 3 liberties" $ 
+            evalState sideStone emptyBoard @?= 3
 
     ]
